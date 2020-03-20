75c18ce85a108a696331cc26dee726442a1c2865
JamesCraster <jamesvaughancraster@gmail.com>
2020-03-20 19:36:09 +0000

    finished rebase

---
+++ index.js
@@ -0,0 +1,106 @@
+var express = require("express");
+const app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.use(express.static("build"));
+let port = 8000;
+http.listen(port, function () {
+  console.log("Port is:" + port);
+});
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.voters = [];
+    this.dead = false;
+  }
+}
+
+class HistoryElement {
+  constructor(string) {
+    this.string = string;
+    this.time = Date().slice(16, 21);
+  }
+}
+
+players = [];
+history = [];
+deadline = "";
+
+io.on("connection", function (socket) {
+  broadcastUpdate = () => {
+    io.emit("updatePlayers", players);
+    io.emit("updateHistory", history);
+  };
+  socket.name = undefined;
+  socket.emit("updatePlayers", players);
+  socket.emit("updateHistory", history);
+  socket.on("addPlayer", function (name) {
+    if (players.filter(elem => elem.name == name).length == 0) {
+      players.push(new Player(name));
+      io.emit("updatePlayers", players);
+    }
+    socket.name = name;
+  });
+  socket.on('toggleDead', function (name) {
+    let target = players.find(elem => elem.name == name);
+    if (target) {
+      target.dead = !target.dead;
+      target.voters = [];
+    }
+    broadcastUpdate();
+  })
+  socket.on("vote", function (name) {
+    if (socket.name != undefined) {
+      let target = players.find(elem => elem.name == name);
+      if (target) {
+        //current voter is not already voting for the target
+        if (target.voters.find(elem => elem == socket.name) == undefined) {
+          //remove current voter from all other targets
+          for (let player of players) {
+            player.voters = player.voters.filter(elem => elem != socket.name);
+          }
+          target.voters.push(socket.name);
+          //add this to history]
+          history.push(
+            new HistoryElement(
+              `${socket.name} has voted for ${target.name} (${
+              target.voters.length
+              })`,
+            ),
+          );
+        } else {
+          //current voter is trying to unvote
+          target.voters = target.voters.filter(elem => elem != socket.name);
+          //add this to history
+          history.push(
+            new HistoryElement(
+              `${socket.name} has unvoted for ${target.name} (${
+              target.voters.length
+              })`,
+            ),
+          );
+        }
+        broadcastUpdate();
+      }
+    }
+  });
+  socket.on("restart", function () {
+    socket.name = undefined;
+    players = [];
+    history = [];
+    broadcastUpdate();
+  });
+  socket.on("clearVotes", function () {
+    for (let player of players) {
+      player.voters = [];
+    }
+    history.push(
+      new HistoryElement(
+        "The votes have been reset",
+      ),
+    );
+    broadcastUpdate();
+  });
+});
--- src/App.js
+++ src/App.js
@@ -8,41 +8,26 @@ import {
   Header,
   Label,
   TextArea,
-  Dimmer,
-  Segment,
 } from "semantic-ui-react";
 import VoteHistory from "./VoteHistory";
 import "./App.css";
-window.socket = io('localhost:8000');
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      socket: window.socket,
-      inRoom: false,
-      named: false,
+      socket: io('localhost:8000'),
     };
-    this.handleRoomSubmit = this.handleRoomSubmit.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.reset = this.reset.bind(this);
     this.clearVotes = this.clearVotes.bind(this);
-    this.state.socket.on("enterRoom", function () {
-      this.setState({ inRoom: "true" });
-    });
   }
   handleSubmit(event) {
     let data = new FormData(event.target);
     if (data.get("name").length != 0) {
-      console.log(data.get("name"));
       this.state.socket.emit("addPlayer", data.get("name"));
       var element = document.getElementById("nameForm");
-      element.style.display = "none";
-    }
-  }
-  handleRoomSubmit(event) {
-    let data = new FormData(event.target);
-    if (data.get("room").length != 0) {
-      this.state.socket.emit("enterRoom", data.get("room"));
+      element.parentNode.removeChild(element);
     }
   }
   clearVotes() {
@@ -64,54 +49,30 @@ class App extends React.Component {
       <div className="App">
         <br />
         <h1>Mafia Voting</h1>
-        <Button color="blue" type="submit">
-          {"Create Room"}
-        </Button>
-        <br></br>
-        <br></br>
-        <Form
-          id="roomForm"
-          onSubmit={this.handleRoomSubmit}
-          style={{ display: this.state.inRoom ? "none" : "default" }}
-        >
+        <Form id="nameForm" onSubmit={this.handleSubmit}>
+          <p>
+            (NB: If you've accidentally refreshed the page, just enter the same
+            name again to regain control of that player)
+          </p>
           <Input
-            maxLength="5"
-            name="room"
-            placeholder="Enter room code"
-          ></Input>
+            maxLength="15"
+            id="name"
+            placeholder="Enter your name:"
+            name="name"
+          />
           <Button type="submit">{"Join"}</Button>
         </Form>
-        <Dimmer.Dimmable dimmed={!this.state.inRoom}>
-          <Dimmer active={false} inverted></Dimmer>
-          <Form
-            id="nameForm"
-            onSubmit={this.handleSubmit}
-            style={{ display: this.state.named ? "none" : "default" }}
-          >
-            <p>
-              (NB: If you've accidentally refreshed the page, just enter the
-              same name again to regain control of that player)
-            </p>
-            <Input
-              maxLength="15"
-              id="name"
-              placeholder="Enter your name:"
-              name="name"
-            />
-            <Button type="submit">{"Join"}</Button>
-          </Form>
-          <br />
-          <Vote socket={this.state.socket} />
-          <br />
-          <VoteHistory socket={this.state.socket} />
-          <br />
-          <Button onClick={this.clearVotes} color="red">
-            Clear Votes
-          </Button>
-          <Button onClick={this.reset} color="red">
-            Reset All
-          </Button>
-        </Dimmer.Dimmable>
+        <br />
+        <Vote socket={this.state.socket} />
+        <br />
+        <VoteHistory socket={this.state.socket} />
+        <br />
+        <Button onClick={this.clearVotes} color="red">
+          Clear Votes
+        </Button>
+        <Button onClick={this.reset} color="red">
+          Reset All
+        </Button>
       </div>
     );
   }
